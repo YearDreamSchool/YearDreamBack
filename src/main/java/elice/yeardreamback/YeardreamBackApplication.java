6426f9e223856f26b947fdb6faa320794de4203f
YachaTree <dnftksghkfkd@icloud.com>
2025-09-08 14:58:59 +0900

    feat: first commit

---
+++ src/main/java/elice/yeardreamback/YeardreamBackApplication.java
@@ -0,0 +1,13 @@
+package elice.yeardreamback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeardreamBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeardreamBackApplication.class, args);
+	}
+
+}
